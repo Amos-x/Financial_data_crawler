b44c082926d92a93b954e4bbd49e1af64ce5335d
Amos <Amos-x@379833553.com>
2018-01-11 19:37:27 +0800

    version-0.5-add-ccmn

---
+++ Data/spiders/ccmn.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from Data.items import ChangjiangXHItem
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
+class CcmnSpider(scrapy.Spider):
+    name = 'ccmn'
+    year = datetime.datetime.today().year
+
+    def start_requests(self):
+        url = 'http://www.ccmn.cn/historyprice/cjxh_1/'
+        yield scrapy.Request(url,callback=self.parse,dont_filter=True)
+
+    def parse(self, response):
+        result = response.css('div#list_elem table tr')[1:]
+        if result:
+            item = ChangjiangXHItem()
+            for group in result:
+                items = [x.strip() for x in group.css('td ::text').extract() if x.strip()]
+                item['web_name'] = 'ccmn'
+                item['date'] = '%s-%s' %(self.year,items[6])
+                item['name'] = items[1]
+                item['aver_price'] = items[3]
+                item['rise_fall'] = items[4]
+                item['unit'] = items[5]
+                item['min_price'],item['max_price'] = items[2].split('—')
+                yield item
+
+if __name__ == '__main__':
+    pass
