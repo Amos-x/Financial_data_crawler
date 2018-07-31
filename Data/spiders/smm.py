33b6f13d2f82db29168e2e08fa0f27b8bc34b050
Amos <Amos-x@379833553.com>
2018-07-31 14:53:47 +0800

    修改smm报价爬虫

--- Data/spiders/smm.py
+++ Data/spiders/smm.py
@@ -15,15 +15,18 @@ class SmmSpider(scrapy.Spider):
         tr_list = response.css('div.content-left-first-pirce tbody tr')
         for tr in tr_list:
             result = [x.strip() for x in tr.css('td ::text').extract()]
+            while '' in result:
+                result.remove('')
             if result[0] == 'SMM 1#电解铜' or result[0] == 'SMM 0#锌锭':
                 item = CnalItem()
                 item['web_name'] = 'cnal'
-                item['name'] = result[0][4:]
-                maxandmix = result[2].split('-')
-                item['min_price'] = maxandmix[0]
-                item['max_price'] = maxandmix[1]
-                item['aver_price'] = result[3]
-                item['rise_fall'] = result[4]
-                toyear = datetime.datetime.today().year
-                item['date'] = '%s-%s' %(toyear,result[5])
-                yield item
+                item['name'] = ('1#铜' if '铜' in result[0] else '0#锌')
+                if result[1] != '--':
+                    maxandmix = result[1].split('-')
+                    item['min_price'] = maxandmix[0]
+                    item['max_price'] = maxandmix[1]
+                    item['aver_price'] = result[2]
+                    item['rise_fall'] = result[3]
+                    toyear = datetime.datetime.today().year
+                    item['date'] = '%s-%s' %(toyear,result[4])
+                    yield item
